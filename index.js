df8c71691a57d0c1f6a18d8e8449afec3325beae
Abdul Rehman <arehman0339@gmail.com>
2025-09-23 17:51:43 +0500

    2

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+// --- Polyfills (must load before app) ---
+import 'assert/';
+import 'browserify-zlib'; // zlib
+import 'https-browserify'; // https
+import 'react-native-url-polyfill/auto'; // URL polyfill
+import 'stream-browserify'; // stream
+import 'stream-http'; // http
+import 'util/'; // util
+
 import './index.native.tsx';
+
