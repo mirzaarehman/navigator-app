b1ab0fe02de69ce10d372b0984c9bb71a35bdcc9
Abdul Rehman <arehman0339@gmail.com>
2025-09-23 17:04:36 +0500

    yarn add stream-http https-browserify url

--- metro.config.js
+++ metro.config.js
@@ -1,18 +1,13 @@
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 const { wrapWithReanimatedMetroConfig } = require('react-native-reanimated/metro-config');
-const path = require('path');
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('@react-native/metro-config').MetroConfig}
- */
 const config = {
   resolver: {
     unstable_enablePackageExports: true,
     extraNodeModules: {
-      url: require.resolve('react-native-url-polyfill'), // 👈 polyfill for axios
+      http: require.resolve('stream-http'),
+      https: require.resolve('https-browserify'),
+      url: require.resolve('url/'),
     },
   },
   transformer: {
