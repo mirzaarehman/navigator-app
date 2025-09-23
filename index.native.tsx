d2ff3e0c80ff5756f3ba3b2b8fdb3b7fa5d6b941
Abdul Rehman <arehman0339@gmail.com>
2025-09-23 16:51:57 +0500

    bug fiing

--- index.native.tsx
+++ index.native.tsx
@@ -1,9 +1,10 @@
 // index.native.tsx
 import { AppRegistry } from 'react-native';
+import 'react-native-gesture-handler';
+import 'react-native-get-random-values';
+import 'react-native-url-polyfill/auto';
 import App from './App';
 import { name as appName } from './app.json';
-import 'react-native-get-random-values';
-import 'react-native-gesture-handler';
 
 // Initialize reanimated
 import 'react-native-reanimated';
--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,6 @@
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 const { wrapWithReanimatedMetroConfig } = require('react-native-reanimated/metro-config');
+const path = require('path');
 
 /**
  * Metro configuration
@@ -10,10 +11,15 @@ const { wrapWithReanimatedMetroConfig } = require('react-native-reanimated/metro
 const config = {
   resolver: {
     unstable_enablePackageExports: true,
+    extraNodeModules: {
+      url: require.resolve('react-native-url-polyfill'), // 👈 polyfill for axios
+    },
   },
   transformer: {
     unstable_allowRequireContext: true,
   },
 };
 
-module.exports = wrapWithReanimatedMetroConfig(mergeConfig(getDefaultConfig(__dirname), config));
+module.exports = wrapWithReanimatedMetroConfig(
+  mergeConfig(getDefaultConfig(__dirname), config)
+);
